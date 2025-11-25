29cdc3dd3c871194530088ffa33462456ee3672b
Dimuthu <Sanjuladimuthu2@gmail.com>
2025-11-25 06:16:02 -0800

    delete 1 file and create 1 file

---
+++ dashboard/src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import logo from "./logo.svg";
+import "./App.css";
+
+const App = () => {
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+}
+
+export default App
